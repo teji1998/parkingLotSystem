a572c05611bfd84ee84d4fe7ca44b77acaf3e528
“teji1998” <tejasvinirpk@gmail.com>
2020-09-10 17:56:44 +0530

    Added:Test case and code to unpark a vehicle

--- src/main/java/parkinglotsystem/ParkingLotSystem.java
+++ src/main/java/parkinglotsystem/ParkingLotSystem.java
@@ -2,7 +2,18 @@ package parkinglotsystem;
 
 public class ParkingLotSystem {
 
-	public boolean parking(Object vehicle) {
+	private Object vehicle;
+
+	public boolean parkingVehicle(Object vehicle) {
+		this.vehicle = vehicle;
 		return true;
+	}
+
+	public boolean unParkingVehicle(Object vehicle) {
+		if(this.vehicle.equals(vehicle)) {
+			this.vehicle = null;
+			return true;
+		}
+		return false;
 	}
 }
--- src/test/java/parkinglotsystem/ParkingLotTest.java
+++ src/test/java/parkinglotsystem/ParkingLotTest.java
@@ -7,9 +7,18 @@ public class ParkingLotTest {
 
 	@Test
 	public void givenAVehicle_WhenParked_ShouldReturnTrue() {
+		Object vehicle = new Object();
 		ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
-		boolean isParked = parkingLotSystem.parking(new Object());
+		boolean isParked = parkingLotSystem.parkingVehicle(vehicle);
 		Assert.assertTrue(isParked);
+	}
 
+	@Test
+	public void givenAVehicle_WhenIsNotParked_ShouldReturnTrue() {
+		Object vehicle = new Object();
+		ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+		parkingLotSystem.parkingVehicle(vehicle);
+		boolean isNotParked = parkingLotSystem.unParkingVehicle(vehicle);
+		Assert.assertTrue(isNotParked);
 	}
 }
