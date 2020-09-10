a00eb765904cf5ba0aa1c056e9fe723edd2240ce
“teji1998” <tejasvinirpk@gmail.com>
2020-09-10 18:15:47 +0530

    Refactored:Test case to handle dry principle

--- src/test/java/parkinglotsystem/ParkingLotTest.java
+++ src/test/java/parkinglotsystem/ParkingLotTest.java
@@ -1,22 +1,28 @@
 package parkinglotsystem;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
 
+	Object vehicle;
+	ParkingLotSystem parkingLotSystem;
+
+	@Before
+	public void setUp() {
+		vehicle = new Object();
+		parkingLotSystem = new ParkingLotSystem();
+	}
+
 	@Test
 	public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-		Object vehicle = new Object();
-		ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
 		boolean isParked = parkingLotSystem.parkingVehicle(vehicle);
 		Assert.assertTrue(isParked);
 	}
 
 	@Test
 	public void givenAVehicle_WhenIsNotParked_ShouldReturnTrue() {
-		Object vehicle = new Object();
-		ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
 		parkingLotSystem.parkingVehicle(vehicle);
 		boolean isNotParked = parkingLotSystem.unParkingVehicle(vehicle);
 		Assert.assertTrue(isNotParked);
